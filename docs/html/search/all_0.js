93aec498f51ceafbd9634428715e3932c05af766
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2020-06-27 21:34:12 -0400

    Delete all_0.js

--- docs/html/search/all_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['apply_20materials_0',['apply materials',['../namespaceapply_01materials.html',1,'']]],
-  ['apply_20materials_2epy_1',['apply materials.py',['../apply_01materials_8py.html',1,'']]],
-  ['apply_5fmaterials_2',['apply_materials',['../namespaceapply_01materials.html#ab4f602048e7e8e80fc7cb6e140ac7c19',1,'apply materials']]]
-];
