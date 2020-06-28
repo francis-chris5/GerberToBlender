daede1e07c17994d36839c3a87bb9110ea637738
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2020-06-27 21:44:45 -0400

    Delete all_4.js

--- docs/html/search/all_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['import_20curves_5',['import curves',['../namespaceimport_01curves.html',1,'']]],
-  ['import_20curves_2epy_6',['import curves.py',['../import_01curves_8py.html',1,'']]],
-  ['import_5fsvg_7',['import_svg',['../namespaceimport_01curves.html#ab519b3256634ad2ec4a4125729869b10',1,'import curves']]]
-];
